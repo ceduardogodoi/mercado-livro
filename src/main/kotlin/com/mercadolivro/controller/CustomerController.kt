75806252f8a6a439cad1fef78d732c990ec2584e
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-01-31 22:37:57 -0300

    31. Refatorando um pouco

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -6,17 +6,25 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("customer")
+@RequestMapping("customers")
 class CustomerController {
 
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel("1", "Gustavo", "email@email.com")
+    fun getCustomer(): List<CustomerModel> {
+        return this.customers
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest) {
-        println(customer)
+        val id = if (this.customers.isEmpty()) {
+            1
+        } else {
+            this.customers.last().id.toInt() + 1
+        }.toString()
+
+        this.customers.add(CustomerModel(id, customer.name, customer.email))
     }
 }
