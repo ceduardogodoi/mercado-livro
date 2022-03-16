329a9e2055bb3c7a04af3d8ef01e966433921603
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-03-15 22:04:24 -0300

    50. Criando outros metodos do Book

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -1,11 +1,17 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostBookRequest
+import com.mercadolivro.controller.request.PutBookRequest
 import com.mercadolivro.extension.toBookModel
+import com.mercadolivro.model.BookModel
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -21,7 +27,34 @@ class BookController(
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody request: PostBookRequest) {
-        val customer = customerService.getById(request.customerId)
+        val customer = this.customerService.getById(request.customerId)
         bookService.create(request.toBookModel(customer))
+    }
+
+    @GetMapping
+    fun findAll(): List<BookModel> {
+        return this.bookService.findAll()
+    }
+
+    @GetMapping("/active")
+    fun findActives(): List<BookModel> =
+        this.bookService.findActives()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): BookModel {
+        return this.bookService.findById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int) {
+        this.bookService.delete(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: Int, @RequestBody book: PutBookRequest) {
+        val bookSaved = this.bookService.findById(id)
+        this.bookService.update(book.toBookModel(bookSaved))
     }
 }
--- src/main/kotlin/com/mercadolivro/service/BookService.kt
+++ src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.enums.BookStatus
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.repository.BookRepository
 import org.springframework.stereotype.Service
@@ -10,7 +11,29 @@ class BookService(
 ) {
 
     fun create(book: BookModel) {
-        bookRepository.save(book)
+        this.bookRepository.save(book)
+    }
+
+    fun findAll(): List<BookModel> {
+        return this.bookRepository.findAll().toList()
+    }
+
+    fun findActives(): List<BookModel> {
+        return this.bookRepository.findByStatus(BookStatus.ATIVO)
+    }
+
+    fun findById(id: Int): BookModel {
+        return this.bookRepository.findById(id).orElseThrow()
+    }
+
+    fun update(book: BookModel) {
+        this.bookRepository.save(book)
+    }
+
+    fun delete(id: Int) {
+        val book = this.findById(id)
+        book.status = BookStatus.CANCELADO
+        this.update(book)
     }
 
 }
