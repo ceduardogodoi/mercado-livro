606deb494690de686ce4e6ddcb66b67d1dacfe01
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-06-21 17:41:41 -0300

    70. Desenvolvendo o Purchase Mapper

--- src/main/kotlin/com/mercadolivro/model/PurchaseModel.kt
+++ src/main/kotlin/com/mercadolivro/model/PurchaseModel.kt
@@ -38,5 +38,5 @@ data class PurchaseModel(
     val price: BigDecimal,
 
     @Column(name = "created_at")
-    val createdAt: LocalDateTime
+    val createdAt: LocalDateTime = LocalDateTime.now()
 )
